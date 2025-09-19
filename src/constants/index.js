b82f0e20ec97ccc6d22dd7e49279c2118eac2376
Naveen Kashyap <nk1910479@gmail.com>
2025-09-19 17:48:19 +0530

    new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,33 +9,13 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2025 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "May - September-(2025)",
+    role: "Front-end Developer",
+    company: "CODEXINTERN.",
+    description: `Built and maintained responsive single-page applications using React.js, JavaScript(ES6+) ,Tailwind CSS, and Framer Motion for animations.Integrated REST APIs and optimized performance for fast, mobile-first user experiences. Implemented smooth UI animations and page transition with framer motion, increasing user engagement.`,
+    technologies: ["Javascript", "React.js", "framer-motion", "tailwindcss"],
   },
-  {
-    year: "2025 - 2024",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2024 - 2023",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2023 - 2022",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  
 ];
 
 export const PROJECTS = [
